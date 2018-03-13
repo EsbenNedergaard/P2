e175e8c10af08345785d55097f917667c6c7efe1
Anton <apalme17@student.aau.dk>
2018-03-13 13:38:31 +0100

    kopieret guiapp

--- src/GraphicalUserInterface/GuiApp.java
+++ src/GraphicalUserInterface/GuiApp.java
@@ -1,4 +1,47 @@
 package GraphicalUserInterface;
 
-public class GuiApp {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class GuiApp extends Application {
+
+    public static final int SCALE = 2;
+    public static final int WIDTH = 80;
+    public static final int HEIGHT = 40;
+    public static final int TILE_SIZE = 5 * SCALE;
+
+    private Group tileGroup = new Group();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Scene scene = new Scene(createContent());
+        primaryStage.setTitle("Warehouse");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public Parent createContent() {
+        Pane root = new Pane();
+        root.setPrefSize(WIDTH * TILE_SIZE, HEIGHT * TILE_SIZE);
+        root.getChildren().add(tileGroup);
+
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                Tile tile = new Tile((x + y) % 2 == 0, x, y);
+                tileGroup.getChildren().add(tile);
+            }
+
+        }
+
+        return root;
+    }
 }
+

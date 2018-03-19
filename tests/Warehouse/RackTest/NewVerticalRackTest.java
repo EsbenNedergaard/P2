d620de3f35bd25cf2486037e11f25a1f98e1c25a
tdybda17 <tdybda17@student.aau.dk>
2018-03-19 20:46:55 +0100

    Skrevet tests for placering af VerticalRack

--- tests/Warehouse/RackTest/NewVerticalRackTest.java
+++ tests/Warehouse/RackTest/NewVerticalRackTest.java
@@ -1,5 +1,7 @@
 package Warehouse.RackTest;
 
+import static Warehouse.GUIWarehouse.HEIGHT_WAREHOUSE;
+import static Warehouse.GUIWarehouse.WIDTH_WAREHOUSE;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import Exceptions.IllegalRackDimensionException;
 import Warehouse.ProductContainer.Rack;
@@ -10,5 +12,28 @@ import org.junit.jupiter.api.Test;
 public class NewVerticalRackTest {
 
     Rack testRack;
+
+    // Testing rack positioning of making a new rack
+    @Test
+    void testNewVerticalRackOutOfWarehouseWidth01() {
+        assertThrows(IllegalRackDimensionException.class, () -> {
+            testRack = new VerticalRack("A", HEIGHT_WAREHOUSE, new Point2D(WIDTH_WAREHOUSE, 0));
+        });
+    }
+
+    @Test
+    void testNewVerticalRackOutOfWarehouseWindow01() {
+        // Placing a rack out of the window
+        assertThrows(IllegalRackDimensionException.class, () -> {
+            testRack = new VerticalRack("A", HEIGHT_WAREHOUSE, new Point2D(-1, -1));
+        });
+    }
+
+    @Test
+    void testNewVerticalRackOutOfWarehouseHeight01() {
+        assertThrows(IllegalRackDimensionException.class, () -> {
+            testRack = new VerticalRack("A", 1, new Point2D(0, HEIGHT_WAREHOUSE));
+        });
+    }
 
 }

0d9942839ec7159e2715612682192fac9c235f3a
EsbenNedergaard <eneder17@student.aau.dk>
2018-03-19 11:40:32 +0100

    Added error msg

--- src/Exceptions/IllegalRackDimensionException.java
+++ src/Exceptions/IllegalRackDimensionException.java
@@ -1,5 +1,18 @@
 package Exceptions;
 
 public class IllegalRackDimensionException extends RuntimeException {
+    private String errorMsg;
 
+    public IllegalRackDimensionException(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+    public IllegalRackDimensionException() {
+        this.errorMsg = "This is not a legal rack dimension";
+    }
+
+    @Override
+    public String toString() {
+        return errorMsg;
+    }
 }

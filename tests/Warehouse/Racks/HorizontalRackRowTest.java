d465e3b3e9a403734323785ee488c923a2a8a387
EsbenNedergaard <eneder17@student.aau.dk>
2018-04-03 10:19:35 +0100

    fixed up tests

--- tests/Warehouse/Racks/HorizontalRackRowTest.java
+++ tests/Warehouse/Racks/HorizontalRackRowTest.java
@@ -12,8 +12,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HorizontalRackRowTest {
     private HorizontalRackRow testRow;
-    private Rack tempRack1;
-    private Rack tempRack2;
+    private Rack rack0;
+    private Rack rack1;
+    private Product product1;
+    private Product product2;
+    private Product product3;
+    private Product product4;
+    private Product product5;
 
     //TODO: SKAL HAVE SAT TESTS OP FOR
     //TODO: Flyttes lavning af produkter herop og kalde dem p1, p2 osv. samt lave vores getters om til package private, da de ikke skal bruges andre steder
@@ -22,8 +27,14 @@ class HorizontalRackRowTest {
         Point2D startPoint = new Point2D(1,1);
         testRow = new HorizontalRackRow(startPoint, 2,2);
 
-        tempRack1 = new Rack(2, new Point2D(1,1));
-        tempRack2 = new Rack(2, new Point2D(2,1));
+        rack0 = new Rack(2, new Point2D(1,1));
+        rack1 = new Rack(2, new Point2D(2,1));
+
+        product1 = new Product("a", 1);
+        product2 = new Product("b", 2);
+        product3 = new Product("c", 3);
+        product4 = new Product("d", 4);
+        product5 = new Product("e", 5);
     }
 
     @Test
@@ -35,76 +46,76 @@ class HorizontalRackRowTest {
     void getRackArray() {
         Rack[] tempArray = testRow.getRackArray();
 
-        assertTrue(tempRack1.equals(tempArray[0]));
-        assertTrue(tempRack2.equals(tempArray[1]));
+        assertTrue(rack0.equals(tempArray[0]));
+        assertTrue(rack1.equals(tempArray[1]));
     }
 
     @Test
     void getRackByIndex() {
-        assertTrue(tempRack1.equals(testRow.getRackByIndex(0)));
-        assertTrue(tempRack2.equals(testRow.getRackByIndex(1)));
+        assertTrue(rack0.equals(testRow.getRackByIndex(0)));
+        assertTrue(rack1.equals(testRow.getRackByIndex(1)));
     }
 
     @Test
     void addProduct1() {
-        testRow.addProduct(new Product("a", 1));
+        testRow.addProduct(product1);
 
-        tempRack1.addProduct(new Product("a", 1));
+        rack0.addProduct(product1);
 
-        assertTrue(tempRack1.equals(testRow.getRackByIndex(0)));
+        assertTrue(rack0.equals(testRow.getRackByIndex(0)));
     }
 
     @Test
     void addProduct2() {
         /*We test more than one product*/
-        testRow.addProduct(new Product("a", 1));
-        testRow.addProduct(new Product("b", 2));
+        testRow.addProduct(product1);
+        testRow.addProduct(product2);
 
-        tempRack1.addProduct(new Product("a", 1));
-        tempRack1.addProduct(new Product("b", 2));
+        rack0.addProduct(product1);
+        rack0.addProduct(product2);
 
-        assertTrue(tempRack1.equals(testRow.getRackByIndex(0)));
+        assertTrue(rack0.equals(testRow.getRackByIndex(0)));
     }
 
     @Test
     void addProduct3() {
         /*We test if it goes into the next rack in the rackArray, when the first is filled up*/
-        testRow.addProduct(new Product("a", 1));
-        testRow.addProduct(new Product("b", 2));
-        testRow.addProduct(new Product("c", 3));
+        testRow.addProduct(product1);
+        testRow.addProduct(product2);
+        testRow.addProduct(product3);
 
-        tempRack2.addProduct(new Product("c", 3));
+        rack1.addProduct(product3);
 
-        assertTrue(tempRack2.equals(testRow.getRackByIndex(1)));
+        assertTrue(rack1.equals(testRow.getRackByIndex(1)));
     }
 
     @Test
     void addProduct4() {
         /*We test for an filled rack*/
-        testRow.addProduct(new Product("a", 1));
-        testRow.addProduct(new Product("b", 2));
-        testRow.addProduct(new Product("c", 3));
-        testRow.addProduct(new Product("d", 4));
+        testRow.addProduct(product1);
+        testRow.addProduct(product2);
+        testRow.addProduct(product3);
+        testRow.addProduct(product4);
 
-        assertThrows(FullRackException.class, ()-> testRow.addProduct(new Product("e", 5)));
+        assertThrows(FullRackException.class, ()-> testRow.addProduct(product5));
     }
 
     @Test
     void addProductToRack1() {
-        testRow.addProductToRack(new Product("a", 1), 1);
+        testRow.addProductToRack(product1, 1);
 
-        tempRack2.addProduct(new Product("a", 1));
+        rack1.addProduct(product1);
 
-        assertTrue(tempRack2.equals(testRow.getRackByIndex(1)));
+        assertTrue(rack1.equals(testRow.getRackByIndex(1)));
     }
 
     @Test
     void addProductToRack2() {
         /*We see if we can get it to throw an exception*/
-        testRow.addProductToRack(new Product("a", 1), 1);
-        testRow.addProductToRack(new Product("b", 2), 1);
+        testRow.addProductToRack(product1, 1);
+        testRow.addProductToRack(product2, 1);
 
-        assertThrows(FullRackException.class, ()-> testRow.addProductToRack(new Product("c", 3), 1));
+        assertThrows(FullRackException.class, ()-> testRow.addProductToRack(product3, 1));
     }
 
 
@@ -124,16 +135,16 @@ class HorizontalRackRowTest {
 
     @Test
     void getAllProductsInRackRow() {
-        testRow.addProduct(new Product("a", 1));
-        testRow.addProduct(new Product("b", 2));
-        testRow.addProduct(new Product("c", 3));
-        testRow.addProduct(new Product("d", 4));
+        testRow.addProduct(product1);
+        testRow.addProduct(product2);
+        testRow.addProduct(product3);
+        testRow.addProduct(product4);
 
         List<Product> tempProductList = testRow.getAllProductsInRackRow();
 
-        assertTrue(tempProductList.get(0).equals(new Product("a", 1)));
-        assertTrue(tempProductList.get(1).equals(new Product("b", 2)));
-        assertTrue(tempProductList.get(2).equals(new Product("c", 3)));
-        assertTrue(tempProductList.get(3).equals(new Product("d", 4)));
+        assertTrue(tempProductList.get(0).equals(product1));
+        assertTrue(tempProductList.get(1).equals(product2));
+        assertTrue(tempProductList.get(2).equals(product3));
+        assertTrue(tempProductList.get(3).equals(product4));
     }
 }

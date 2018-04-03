e4fc1c90fae88480aefd2170abb9d02983ee0386
Andreas <atorp17@student.aau.dk>
2018-04-03 09:27:25 +0200

    Simon told me to

--- src/Geometry/Graph.java
+++ src/Geometry/Graph.java
@@ -3,41 +3,68 @@ package Geometry;
 import java.util.*;
 
 public class Graph {
-
+    private ArrayList<Node> closedSet = new ArrayList<>();
     private SortedSet<Node> openSet = new TreeSet<>(new NodeComparator());
-    private Map<Node, Node> cameFrom = new HashMap<>();
+    private ArrayList<Node> allNodes;
 
-    private Set<Node> allNodes;
-
-    public Graph(Set<Node> allNodes) {
+    public Graph(ArrayList<Node> allNodes) {
         this.allNodes = allNodes;
-
-
     }
 
-    public List<Node> findShortestRoute(Node start, Node end){
+    public ArrayList<Node> findShortestRoute(Node start, Node end){
         for(Node node : allNodes){
-
-            node.setDistanceToInf();
-
+            if (node.equals(start)) {
+                node.setDistanceFromStart(0);
+                openSet.add(node);
+            }
+            else{
+                node.setDistanceToInf();
+            }
             node.setDistanceToEnd(node);
-
+            node.setNeighbourNodes(allNodes);
         }
-
-        start.setDistanceFromStart(0);
-
-        openSet.add(start);
 
         while(!openSet.isEmpty()){
             Node current = openSet.first();
 
             if(current.equals(end)){
-                return generateRoute();
+                end = current;
+                break;
             }
+            openSet.remove(current);
+            closedSet.add(current);
 
+            for (Node neighbour : current.getNeighbourNodes()){
+                if (closedSet.contains(neighbour)){
+                    continue;
+                }
+                openSet.add(neighbour);
 
+                //A better path exists
+                if(current.getDistanceFromStart() + 1 <= neighbour.getDistanceFromStart()){
+                    neighbour.setCameFrom(current);
+                    neighbour.setDistanceFromStart(current.getDistanceFromStart() + 1);
+                }
+            }
         }
-
+        return constructPath(start, end);
     }
 
+    private ArrayList<Node> constructPath(Node start, Node end){
+
+        ArrayList<Node> path = new ArrayList<>();
+
+        Node next = end;
+
+        while(!next.equals(start)){
+            path.add(next);
+            next = next.getCameFrom();
+        }
+
+        path.add(start);
+
+        Collections.reverse(path);
+
+        return path;
+    }
 }
--- src/Geometry/Node.java
+++ src/Geometry/Node.java
@@ -1,28 +1,73 @@
 package Geometry;
 
-public class Node extends Point2D{
-    private static final int INFINITY = 1000000;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
+public class Node extends Point2D {
+    private static final int INFINITY = 1000000;
     private int distanceFromStart;
     private int distanceToEnd;
+    private Node cameFrom;
+    private ArrayList<Node> neighbourNodes;
 
-    public Node(){
+    public Node(Point2D p) {
+        super(p);
+    }
 
+    public int getDistanceFromStart() {
+        return distanceFromStart;
+    }
+
+    public ArrayList<Node> getNeighbourNodes() {
+        return neighbourNodes;
+    }
+
+    public Node getCameFrom() {
+        return cameFrom;
+    }
+
+    public void setCameFrom(Node cameFrom) {
+        this.cameFrom = cameFrom;
     }
 
     public void setDistanceFromStart(int distanceFromStart) {
         this.distanceFromStart = distanceFromStart;
     }
 
-    public void setDistanceToInf(){
+    public void setDistanceToInf() {
         this.distanceFromStart = INFINITY;
     }
 
-    public void setDistanceToEnd(Node endNode){
+    public void setDistanceToEnd(Node endNode) {
         int Xdistance = Math.abs(endNode.getX() - this.getX());
         int Ydistance = Math.abs(endNode.getY() - this.getY());
 
         this.distanceToEnd = Xdistance + Ydistance;
+    }
+
+    public void setNeighbourNodes(ArrayList<Node> allNodes) {
+        neighbourNodes = new ArrayList<Node>();
+        for (Node node : allNodes) {
+            if (isNeighbour(node)) {
+                neighbourNodes.add(node);
+            }
+        }
+    }
+
+    private boolean isNeighbour(Node node) {
+        if (this.getX() == node.getX() + 1 && this.getY() == node.getY()) {
+            return true;
+        } else if (this.getX() == node.getX() - 1 && this.getY() == node.getY()) {
+            return true;
+        } else if (this.getX() == node.getX() && this.getY() == node.getY() + 1) {
+            return true;
+        } else if (this.getX() == node.getX() && this.getY() == node.getY() - 1) {
+            return true;
+        }
+
+        return false;
     }
 
     public int getTotalDistance() {

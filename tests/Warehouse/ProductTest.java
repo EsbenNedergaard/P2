9657477113ffd464f8f2535309d7327534ff1bc8
EsbenNedergaard <eneder17@student.aau.dk>
2018-04-05 13:03:00 +0200

    product tests

--- tests/Warehouse/ProductTest.java
+++ tests/Warehouse/ProductTest.java
@@ -1,35 +1,44 @@
 package Warehouse;
 
 import Exceptions.FullRackException;
+import Geometry.Point2D;
+import Warehouse.Racks.Rack;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
-    Product testProduct;
-    Product product1;
-    Product product2;
+    private Product testProduct;
+    private Product product1;
+    private Product product2;
+    private Rack rack;
 
     @BeforeEach
     void beforeEach() {
         testProduct = new Product(1);
         product1 = new Product(1);
         product2 = new Product(2);
+        rack = new Rack(2, new Point2D(1,1));
     }
 
 
-
+    //GIVER FØRST MENING NÅR VI HAR EXCEPTIONS
     @Test
     void setRack() {
+
+
     }
 
     @Test
-    void getId() {
+    void getId1() {
+        assertEquals(1, testProduct.getId());
     }
 
     @Test
     void getRack() {
+        testProduct.setRack(rack);
+        assertEquals(rack, testProduct.getRack());
     }
 
     @Test

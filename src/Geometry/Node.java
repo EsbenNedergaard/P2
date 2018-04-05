c2fc550349df7fa7ae7cb605e4394e5b7bce3957
Andreas <atorp17@student.aau.dk>
2018-04-05 15:12:04 +0200

    added test with obstacles (some only at certain times)

--- src/Geometry/Node.java
+++ src/Geometry/Node.java
@@ -1,13 +1,14 @@
 package Geometry;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 /*TODO: få lavet et nabo-system i forhold til vores varehus, som ved hvilke punkter man kan gå fra og til, da det gør vi kan være ligeglade
   med, hvilke punkter vi ikke kan gå igennem, hvis vi bare har ende punkterne af gangene, og kan afgøre, fra hvilke punkter man kan gå til andre punkter,
   nemmeste ville nok være at lave en liste af "right end points" og en liste af "left end points" også kan bare tjekke om varens y-koordinat stemmer overens med
   et endepunkt, så kører vi bare igennem et punkt efter et andet, og tager dem med lavest f-værdi som er naboer til vores nuværende punkt indtil vi når slutpunktet  */
 
-public class Node extends Point2D implements Comparable<Node> {
+public class Node extends Point2D {
     private static final int INFINITY = 1000000;
     private int distanceFromStart;
     private int distanceToEnd;
@@ -78,7 +79,7 @@ public class Node extends Point2D implements Comparable<Node> {
                 return true;
             } else if (this.getX() == node.getX() && this.getY() == node.getY() - 1) {
                 return true;
-            } else if (this.getX() == node.getY() && this.getY() == node.getY()) {
+            } else if (this.getX() == node.getX() && this.getY() == node.getY()) {
                 return true;
             }
         }
@@ -90,8 +91,17 @@ public class Node extends Point2D implements Comparable<Node> {
     }
 
     @Override
-    public int compareTo(Node that) {
-        NodeComparator myComp = new NodeComparator();
-        return myComp.compare(this, that);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Node node = (Node) o;
+        return this.getTime() == node.getTime();
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(super.hashCode(), getTime());
     }
 }
--- tests/Geometry/GraphTest.java
+++ tests/Geometry/GraphTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 class GraphTest {
-    private final int GRID_HEIGHT = 42;
-    private final int GRID_LENGTH = 16;
-    private final int MAX_TIME = 50;
+    private final int GRID_HEIGHT = 10;
+    private final int GRID_LENGTH = 10;
+    private final int MAX_TIME = 100;
 
 
     @Test
@@ -16,12 +16,14 @@ class GraphTest {
         for (int k = 0; k < MAX_TIME; k++) {
             for (int i = 0; i < GRID_HEIGHT; i++) {
                 for (int j = 0; j < GRID_LENGTH; j++) {
-                    inputSet.add(new Node(new Point2D(i, j), k));
+                    if (!(i == 8 && j > 4) && !(i == 1 && j < 5) && !(i == 3 && j > 1) && !(i == 1 && j == 5  && k == 6) && !(i == 0 && j == 3  && k == 4) && !(i == 1 && j == 6  && k == 7) && !(i == 1 && j == 7  && k == 8) && !(i == 1 && j == 8  && k == 9) && !(i == 1 && j == 9  && k == 10)) {
+                        inputSet.add(new Node(new Point2D(j, i), k));
+                    }
                 }
             }
         }
 
-        Node startNode = new Node(new Point2D(0, 0));
+        Node startNode = new Node(new Point2D(0, 0), 0);
         Node endNode = new Node(new Point2D(GRID_HEIGHT - 1, GRID_LENGTH - 1));
 
         Graph testGraph = new Graph(inputSet);

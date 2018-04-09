029481705183df6e7f71678ce1dade80180aff49
Anton <apalme17@student.aau.dk>
2018-04-09 14:31:36 +0200

    filled racks with products

--- tests/Warehouse/Warehouse22bTest.java
+++ tests/Warehouse/Warehouse22bTest.java
@@ -52,17 +52,19 @@ class Warehouse22bTest {
 
     @Test
     void getPickingPoints() {
-        testWarehouse.getAisleList().get(0).getFirstRackRow().addProduct(new Product(1));
-        testWarehouse.getAisleList().get(1).getSecondRackRow().getRackByIndex(5).addProduct(new Product(2));
+        //testWarehouse.getAisleList().get(0).getFirstRackRow().addProduct(new Product(1));
+        //testWarehouse.getAisleList().get(1).getSecondRackRow().getRackByIndex(5).addProduct(new Product(2));
 
         List<Product> productPickList = new ArrayList<>();
         productPickList.add(testWarehouse.getAisleList().get(0).getFirstRackRow().getRackByIndex(0).getProduct(0));
         productPickList.add(testWarehouse.getAisleList().get(1).getSecondRackRow().getRackByIndex(5).getProduct(0));
+        productPickList.add(testWarehouse.getAisleList().get(2).getSecondRackRow().getRackByIndex(5).getProduct(0));
 
 
         for (Point2D pickPoint : testWarehouse.getPickingPoints(productPickList)) {
             System.out.println(pickPoint.getX() + ", " + pickPoint.getY());
         }
+
     }
 
 }

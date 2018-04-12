a32ea0aec363ed295e544a16c9f594a95ad8017b
EsbenNedergaard <eneder17@student.aau.dk>
2018-04-12 14:43:25 +0200

    added few tests for SpaceTimeGrid

--- src/Geometry/Node.java
+++ src/Geometry/Node.java
@@ -32,7 +32,8 @@ public class Node extends Point2D {
 
     private boolean isNeighbour(Node node) {
         //TODO: få lavet et tjek om den indsendte node er i det næste lag
-        //if (this.getTime() + 1 == node.getTime()) {
+
+        if (this.getTime() + 1 == node.getTime()) {
             if (this.getX() == node.getX() + 1 && this.getY() == node.getY()) {
                 return true;
             } else if (this.getX() == node.getX() - 1 && this.getY() == node.getY()) {
@@ -43,9 +44,8 @@ public class Node extends Point2D {
                 return true;
             } else if (this.getX() == node.getX() && this.getY() == node.getY()) {
                 return true;
-
             }
-        //}
+        }
         return false;
     }
 

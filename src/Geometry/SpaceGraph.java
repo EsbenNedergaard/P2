c2f8738a82205330bd04e3f4a32a89c0fa13ecf5
EsbenNedergaard <eneder17@student.aau.dk>
2018-04-12 10:48:17 +0200

    setting up SpaceGraph

--- src/Geometry/SpaceGraph.java
+++ src/Geometry/SpaceGraph.java
@@ -1,10 +1,35 @@
 package Geometry;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SpaceGraph {
     private List<NodeLayer> nodeLayerList;
+    private int maxTime;
 
+    public SpaceGraph(BaseLayer baseLayer, int maxTime) {
+        this.nodeLayerList = new ArrayList<>();
+        this.maxTime = maxTime;
 
+        setupNodeLayerList(baseLayer);
+    }
 
+    private void setupNodeLayerList(BaseLayer baseLayer) {
+        for (int i = 0; i < maxTime; i++) {
+            NodeLayer tempNodeLayer = new NodeLayer(baseLayer.getNodeList(), i);
+            nodeLayerList.add(tempNodeLayer);
+            if (i != 0) {
+                nodeLayerList.get(i-1).setAllNeighbourNodesForLayer(nodeLayerList.get(i));
+            }
+        }
+    }
+
+    public List<Node> getAllNodes(){
+        List<Node> allNodes = new ArrayList<>();
+        for(NodeLayer element : nodeLayerList){
+            allNodes.addAll(element.getNodeList());
+        }
+
+        return allNodes;
+    }
 }

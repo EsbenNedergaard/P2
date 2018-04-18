82872a60348a06b937d112ef3e5ae91786ae3529
simkort999 <sim465vman@live.dk>
2018-04-18 14:41:03 +0200

    Test til NodeComparator. + FastestRoute begyndt

---
+++ src/Geometry/FastestRoute.java
@@ -0,0 +1,47 @@
+package Geometry;
+
+import Exceptions.RouteNotPossibleException;
+
+import java.util.List;
+
+public class FastestRoute {
+    private final Node startNode = new Node(new Point2D(0, 0));
+    private List<Point2D> pickingList;
+    private SpaceTimeGrid grid;
+    private List<Node> fastestRoute;
+    private List<Node> tempFastestRoute;
+
+    public FastestRoute(List<Point2D> pickingList, SpaceTimeGrid grid) {
+        this.pickingList = pickingList;
+        this.grid = grid;
+        fastestRoute.add(startNode);
+    }
+
+    public List<Node> getFastestRoute(){
+
+        PathFinder path = new PathFinder(grid);
+        Node endNode = new Node(pickingList.get(0));
+
+
+        try {
+            int listSize = pickingList.size();
+
+            for(int i = 0; i < listSize; i++) {
+
+                tempFastestRoute = path.findShortestRoute(startNode, endNode);
+
+                if(tempFastestRoute.size() < fastestRoute.size()){
+                    fastestRoute = tempFastestRoute;
+                }
+            }
+            fastestRoute.add(startNode);
+
+        } catch (RouteNotPossibleException e) {
+            e.getMessage();
+        }
+
+
+
+        return fastestRoute;
+    }
+}
--- tests/Geometry/NodeComparatorTest.java
+++ tests/Geometry/NodeComparatorTest.java
@@ -12,27 +12,84 @@ class NodeComparatorTest {
 
     // Ved ikke helt hvad der skal testes.
     @Test
-    void testMethod_compare(){
+    void testMethod_compare_01(){
         Node firstNode = new Node(new Point2D(1, 1));
         Node secondNode = new Node(new Point2D(2, 2));
-        Node thirdNode = new Node(new Point2D(1, 1));
-        Node fourthNode = new Node(new Point2D(1,2));
+
 
         Node endNode = new Node(new Point2D(4,4));
 
         firstNode.setDistanceToEnd(endNode);
         secondNode.setDistanceToEnd(endNode);
-        thirdNode.setDistanceToEnd(endNode);
-        fourthNode.setDistanceToEnd(endNode);
+
 
         // Let starting node be (0,0)
         firstNode.setDistanceFromStart(2);
         secondNode.setDistanceFromStart(4);
-        thirdNode.setDistanceFromStart(2);
-        fourthNode.setDistanceFromStart(3);
+
 
         NodeComparator compareElement = new NodeComparator();
 
         assertTrue(compareElement.compare(firstNode, secondNode) == 0);
+
+    }
+
+    @Test
+    void testMethod_compare_02(){
+        Node firstNode = new Node(new Point2D(1, 1));
+        Node secondNode = new Node(new Point2D(1,2));
+
+        Node endNode = new Node(new Point2D(4,4));
+
+        firstNode.setDistanceToEnd(endNode);
+        secondNode.setDistanceToEnd(endNode);
+
+        // Let starting node be (0,0)
+        firstNode.setDistanceFromStart(2);
+        secondNode.setDistanceFromStart(3);
+
+        NodeComparator compareElement = new NodeComparator();
+
+        assertTrue(compareElement.compare(firstNode, secondNode) == 0);
+    }
+
+    @Test
+    void testMethod_compare_03(){
+        Node firstNode = new Node(new Point2D(1,2));
+        Node secondNode = new Node(new Point2D(4, 0));
+
+        Node endNode = new Node(new Point2D(1, 4));
+
+        firstNode.setDistanceToEnd(endNode);
+        secondNode.setDistanceToEnd(endNode);
+
+        // Let starting node be (0,0)
+        firstNode.setDistanceFromStart(3);
+        secondNode.setDistanceFromStart(4);
+
+        NodeComparator compareElement = new NodeComparator();
+
+        assertTrue(compareElement.compare(firstNode, secondNode) < 0);
+
+    }
+
+    @Test
+    void testMethod_compare_04(){
+        Node firstNode = new Node(new Point2D(1,2));
+        Node secondNode = new Node(new Point2D(4, 0));
+
+        Node endNode = new Node(new Point2D(1, 4));
+
+        firstNode.setDistanceToEnd(endNode);
+        secondNode.setDistanceToEnd(endNode);
+
+        // Let starting node be (0,0)
+        firstNode.setDistanceFromStart(3);
+        secondNode.setDistanceFromStart(4);
+
+        NodeComparator compareElement = new NodeComparator();
+
+        assertTrue(compareElement.compare(secondNode, firstNode) > 0);
+
     }
 }

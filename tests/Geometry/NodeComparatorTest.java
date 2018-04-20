09c5b62f60be19942da290c004db906905bcb482
sujee <ssrika17@student.aau.dk>
2018-04-20 10:40:56 +0200

    comparator og compare til nodes - nu med tests

--- tests/Geometry/NodeComparatorTest.java
+++ tests/Geometry/NodeComparatorTest.java
@@ -30,13 +30,13 @@ class NodeComparatorTest {
 
         NodeComparator compareElement = new NodeComparator();
 
-        assertTrue(compareElement.compare(firstNode, secondNode) == 0);
+        assertTrue(compareElement.compare(firstNode, secondNode) > 0);
 
     }
 
     @Test
     void testMethod_compare_02(){
-        Node firstNode = new Node(new Point2D(1, 1));
+        Node firstNode = new Node(new Point2D(1, 3));
         Node secondNode = new Node(new Point2D(1,2));
 
         Node endNode = new Node(new Point2D(4,4));
@@ -45,12 +45,12 @@ class NodeComparatorTest {
         secondNode.setDistanceToEnd(endNode);
 
         // Let starting node be (0,0)
-        firstNode.setDistanceFromStart(2);
-        secondNode.setDistanceFromStart(3);
+        firstNode.setDistanceFromStart(3);
+        secondNode.setDistanceFromStart(2);
 
         NodeComparator compareElement = new NodeComparator();
 
-        assertTrue(compareElement.compare(firstNode, secondNode) == 0);
+        assertTrue(compareElement.compare(firstNode, secondNode) < 0);
     }
 
     @Test
@@ -58,13 +58,13 @@ class NodeComparatorTest {
         Node firstNode = new Node(new Point2D(1,2));
         Node secondNode = new Node(new Point2D(4, 0));
 
-        Node endNode = new Node(new Point2D(1, 4));
+        Node endNode = new Node(new Point2D(4, 4));
 
         firstNode.setDistanceToEnd(endNode);
         secondNode.setDistanceToEnd(endNode);
 
         // Let starting node be (0,0)
-        firstNode.setDistanceFromStart(3);
+        firstNode.setDistanceFromStart(2);
         secondNode.setDistanceFromStart(4);
 
         NodeComparator compareElement = new NodeComparator();
@@ -78,18 +78,38 @@ class NodeComparatorTest {
         Node firstNode = new Node(new Point2D(1,2));
         Node secondNode = new Node(new Point2D(4, 0));
 
-        Node endNode = new Node(new Point2D(1, 4));
+        Node endNode = new Node(new Point2D(4, 4));
 
         firstNode.setDistanceToEnd(endNode);
         secondNode.setDistanceToEnd(endNode);
 
         // Let starting node be (0,0)
         firstNode.setDistanceFromStart(3);
-        secondNode.setDistanceFromStart(4);
+        secondNode.setDistanceFromStart(2);
 
         NodeComparator compareElement = new NodeComparator();
 
-        assertTrue(compareElement.compare(secondNode, firstNode) > 0);
+        assertTrue(compareElement.compare(firstNode, secondNode) > 0);
 
+    }
+
+
+    @Test
+    void testMethod_compare_05() {
+        Node firstNode = new Node(new Point2D(3,2));
+        Node secondNode = new Node(new Point2D(2, 2));
+
+        Node endNode = new Node(new Point2D(4, 4));
+
+        firstNode.setDistanceToEnd(endNode);
+        secondNode.setDistanceToEnd(endNode);
+
+        // Let starting node be (0,0)
+        firstNode.setDistanceFromStart(3);
+        secondNode.setDistanceFromStart(2);
+
+        NodeComparator compareElement = new NodeComparator();
+
+        assertTrue(compareElement.compare(firstNode, secondNode) == 0);
     }
 }

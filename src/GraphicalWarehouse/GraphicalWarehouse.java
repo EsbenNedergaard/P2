6bf560ac8939e4b55804375c89f4f12cbe8d1b98
tdybda17 <tdybda17@student.aau.dk>
2018-04-20 13:45:16 +0200

    Tobias ny

--- src/GraphicalWarehouse/GraphicalWarehouse.java
+++ src/GraphicalWarehouse/GraphicalWarehouse.java
@@ -5,7 +5,6 @@ import Geometry.*;
 import Geometry.Node;
 import GraphicalWarehouse.GraphicalObjects.*;
 import GraphicalWarehouse.InteractionHandler.InputFieldDataHandler;
-import Warehouse.Aisle.Aisle;
 import Warehouse.Racks.*;
 import Warehouse.*;
 import javafx.animation.AnimationTimer;
@@ -30,7 +29,6 @@ public class GraphicalWarehouse {
     private Warehouse warehouse;
     private int LENGTH_WAREHOUSE;
     private int WIDTH_WAREHOUSE;
-
     // Path finder
     private FastestRoute pathFinder;
     private final int MAX_TIME = 500;
@@ -43,13 +41,8 @@ public class GraphicalWarehouse {
 
     private List<OrderPickerGraphics> orderPickerList;
 
-    private InteractionGraphics interactionGraphics = new InteractionGraphics();
-
-    private GenerateRandomPickingRoute
-            randomPickingRoute = new GenerateRandomPickingRoute();
-
-    // Animation timer
-    private AnimationTimer timer;
+    // Animation programTimer
+    private AnimationTimer programTimer;
     private int UPDATE_COUNTER = 0;
 
     public GraphicalWarehouse(Warehouse warehouse) {
@@ -133,17 +126,21 @@ public class GraphicalWarehouse {
 
     private Group getInteractionFieldGroup() {
 
+        InteractionGraphics interactionGraphics = new InteractionGraphics();
         BorderPane borderPane = new BorderPane();
         GridPane gridpane = new GridPane();
+
         gridpane.setMinWidth(TILE_SIZE * WIDTH_WAREHOUSE * SCALE);
         gridpane.setHgap(10);
         Label heading = new Label("Add product list to queue. Product id separated by comma");
         heading.getStyleClass().add("heading-label");
 
+        // Get interaction objects
         Button addRouteButton = interactionGraphics.getAddDataButton();
         Button launchButton = interactionGraphics.getLaunchButton();
         TextField inputField = interactionGraphics.getInputField();
 
+        // Set objects in GridPane
         gridpane.add(heading, 1, 1, 5, 1);
         gridpane.add(inputField, 1, 2, 4, 2);
         gridpane.add(addRouteButton, 5, 2);
@@ -152,42 +149,50 @@ public class GraphicalWarehouse {
         TableGraphics table = (TableGraphics) interactionGraphics.getTable();
 
         borderPane.getStyleClass().add("interaction-border-pane");
-
         borderPane.setRight(gridpane);
         borderPane.setLeft(table);
 
-        setOnButtonClickEvent(addRouteButton, inputField, table);
+        // Set event listeners
+        setOnButtonClickEvent(addRouteButton, inputField, table, launchButton);
 
         return new Group(borderPane);
 
     }
 
-    private void setOnButtonClickEvent(Button addButton, TextField inputField, TableGraphics table) {
-
+    private void setOnButtonClickEvent(Button addButton, TextField inputField, TableGraphics table, Button launchButton) {
+        // Run the same method on button clicked and ENTER pressed
         addButton.setOnMouseClicked(e -> this.actionsForAddProductIDs(inputField, table));
         inputField.setOnKeyPressed(e -> {
             if(e.getCode() == KeyCode.ENTER) {
                 this.actionsForAddProductIDs(inputField, table);
             }
         });
+
+        launchButton.setOnMouseClicked(e -> programTimer.start());
     }
 
     private void actionsForAddProductIDs(TextField inputField, TableGraphics table) {
+
+        // TODO: Create a new task for the heavy calculations
+        // TODO: Put all routes into a list and calculate them in another method
+
         InputFieldDataHandler textHandler = new InputFieldDataHandler();
+        // Get the id list from the input field
         List<Integer> tempProductIDList = textHandler.generateProductIDList(inputField.getText());
-        // Find route for picker
-        List<Point2D> nodeList = this.warehouse.getPickingPointsFromIDs(tempProductIDList);
-        List<Node> fastestRoute = this.pathFinder.calculateBestRoute(nodeList);
+
+        List<Point2D> nodeList = warehouse.getPickingPointsFromIDs(tempProductIDList);
+        List<Node> fastestRoute = pathFinder.calculateBestRoute(nodeList);
 
         OrderPickerGraphics orderPicker = new OrderPickerGraphics(fastestRoute);
         addPicker(orderPicker);
 
-        // Add to table
+        // Add to graphical ID table
         String generatedProductIDs = textHandler.generateProductIDString();
 
         if(!generatedProductIDs.equals(""))
             table.add(generatedProductIDs);
 
+        // Clear the input field when done
         inputField.clear();
     }
 
@@ -212,13 +217,12 @@ public class GraphicalWarehouse {
 
         root.getChildren().addAll(borderPane);
 
-        timer = new AnimationTimer() {
+        programTimer = new AnimationTimer() {
             @Override
             public void handle(long now) {
                 onUpdate();
             }
         };
-        timer.start();
 
         return root;
     }

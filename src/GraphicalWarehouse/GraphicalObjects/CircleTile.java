0c1daba40249a0807ca9a3f58b16ee86f963be57
tdybda17 <tdybda17@student.aau.dk>
2018-04-24 12:30:02 +0200

    UPDATES:
    - It is now possible to view (highlight) picking routes

--- src/GraphicalWarehouse/GraphicalObjects/CircleTile.java
+++ src/GraphicalWarehouse/GraphicalObjects/CircleTile.java
@@ -3,7 +3,6 @@ package GraphicalWarehouse.GraphicalObjects;
 import Geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 
 import static Warehouse.GUIWarehouse.TILE_SIZE;
 
@@ -14,12 +13,11 @@ public class CircleTile extends Circle {
     public CircleTile(Point2D point) {
 
         // Pixel width of tile
-        setRadius(TILE_SIZE / 4.8);
-        setFill(Color.valueOf("#4ed480"));
-        setOpacity(1);
+        setRadius(TILE_SIZE / 5.5);
+        setFill(Color.valueOf("#3b3b3b"));
 
         // Graphical position of tile
-        relocate(point.getXPixels() + 7.5, point.getYPixels() + 7.5);
+        relocate(point.getXPixels() + 8, point.getYPixels() + 8);
 
     }
 

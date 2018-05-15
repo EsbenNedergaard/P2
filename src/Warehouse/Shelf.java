677e134729f704e6bfe4789ffe9420a645ee6df2
simkort999 <sim465vman@live.dk>
2018-05-15 10:22:41 +0200

    Shelf opdateret

--- src/Warehouse/Shelf.java
+++ src/Warehouse/Shelf.java
@@ -1,11 +1,18 @@
 package Warehouse;
 
 
+import java.util.List;
+
 public class Shelf {
     private Product product;
+    private List<Product> productList;
 
     public Shelf(Product product) {
         this.product = product;
+    }
+
+    public Shelf(List<Product> productList){
+        this.productList = productList;
     }
 
     public Shelf() {
@@ -17,6 +24,11 @@ public class Shelf {
 
     public Product getProduct() {
         return product;
+    }
+
+    // works as getter and setter
+    public List<Product> getProductList(){
+        return this.productList;
     }
 
     public boolean containsProduct() {

24b3b877c184abc6cea7acb75c2573722a1eea64
EsbenNedergaard <eneder17@student.aau.dk>
2018-05-16 10:31:02 +0200

    True Distance test and upgrades to 7x7 to be more dynamic

--- src/WarehouseSimulation/WarehouseSimulation.java
+++ src/WarehouseSimulation/WarehouseSimulation.java
@@ -57,8 +57,8 @@ public class WarehouseSimulation {
 
         Point2D routeStartPoint = warehouse.getRouteStartPoint();
         Point2D routeEndPoint = warehouse.getRouteEndPoint();
-        //this.routeFinder = new RouteFinder(new FastestPathFinder(grid), routeStartPoint, routeEndPoint);
-        this.routeFinder = new RouteFinder(new ShortestPathFinder(grid), routeStartPoint, routeEndPoint);
+        this.routeFinder = new RouteFinder(new FastestPathFinder(grid), routeStartPoint, routeEndPoint);
+        //this.routeFinder = new RouteFinder(new ShortestPathFinder(grid), routeStartPoint, routeEndPoint);
     }
 
     private void addPicker(OrderPickerGraphic picker) {
